60c945a4f429344a87aba11b88e283689752878b
manikandanshiva001 <mani423r@gmail.com>
2023-12-30 11:34:02 +0530

    change 001

--- tr_1/settings.py
+++ tr_1/settings.py
@@ -31,7 +31,7 @@ EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 STATICFILES_DIRS=[
 'C:/Users/Admin/PycharmProjects/tr_1/static']
